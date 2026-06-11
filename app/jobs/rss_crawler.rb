ff55a10a0dede67553fb0ec8d4a54c50aacf6556
Ameco-dev <psnw.adrien@gmail.com>
2026-06-11 20:45:32 +0200

    feat: complete RssCrawler with type mapping and error handling

--- app/jobs/rss_crawler.rb
+++ app/jobs/rss_crawler.rb
@@ -5,16 +5,21 @@ class RssCrawler
 
   def fetch
     @feed = Feedjira::Feed.fetch_and_parse(@source.url)
+
+
     items = @feed.items
+    items_type = { "article" => "Article", "podcast" => "Episode", "video" => "Video" }
     items.each do |item|
       Item.find_or_create_by(url: item.url) do |post|
         post.source = @source
         post.url = item.url
         post.title = item.title
-        post.type = @source.source_type
+        post.type = items_type[@source.source_type]
         post.author = item.author
         post.duration = item.duration
       end
     end
+    rescue => exception
+      warn exception.message
   end
 end
