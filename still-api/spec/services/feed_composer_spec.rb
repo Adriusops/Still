6efb3235aa93ad6f035e56298453bdc40d91a3b9
Ameco-dev <psnw.adrien@gmail.com>
2026-06-25 13:35:17 +0200

    Fix: Refactoring feed composer test with more readable code

--- still-api/spec/services/feed_composer_spec.rb
+++ still-api/spec/services/feed_composer_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe FeedComposer do
   let(:items) { [ Item.new(source_id: 1), Item.new(source_id: 2), Item.new(source_id: 1) ] }
 
-  it "compose les items par source_id sans doublons" do
-    composer  = FeedComposer.compose(items).each_cons(2).all? { |a, b| a.source_id != b.source_id }
-    expect(composer).to be_truthy
+  it "compose les items par source_id sans doublons consécutifs" do
+    result = FeedComposer.compose(items)
+
+    result.each_cons(2) do |a, b|
+        expect(a.source_id).not_to eq(b.source_id)
+      end
   end
 end
