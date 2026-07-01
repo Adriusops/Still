a5bce40b245d57727069b2f5a3c31fc8deae2439
Ameco-dev <psnw.adrien@gmail.com>
2026-07-01 14:09:15 +0200

    fix: invert feed items order

--- still-api/app/controllers/api/v1/items_controller.rb
+++ still-api/app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-      items = current_user.items.includes(:source).order(created_at: :desc)
+      items = current_user.items.includes(:source).order(created_at: :asc)
       render json: FeedComposer.compose(items).map { |i|
         i.as_json.merge(
           type: i.type,
